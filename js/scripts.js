7edaa40652d7eadb169b8600a0fd614b70616f2c
mauro-mustillo <mmustillo@shift3tech.com>
2018-05-23 22:51:58 -0700

    Day2

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,34 @@
-var myName = "My Name is Mauro and I want to learn Javascript. My Favorite thing is to Play VideoGames";
-console.log(myName);
+var myName = "Mauro";
+let age = new Date('10.06.1974');
+const goal = "learn js";
+var example = Math.ceil(8.5)
+console.log(myName , age, goal);
+console.log(example )
+/*
+let userAnswer = confirm("Did you do that");
+console.log(userAnswer);
+
+let userAnswer2 = prompt("Did you do that", "Yes");
+console.log(userAnswer2);
+*/
+
+/*
+Declare 2 let
+Favorite movie
+favorite food
+
+Declare 2 const
+Year of the movie
+food ingredients
+
+Alert with favorite food ingredients
+Confirm movie and Year  */
+
+let favMovie = "Goodfellas";
+let favFood = "pastas";
+const favMovieYear = new Date(5/21/1990);
+const favFoodIngredients = ["sauce, pesto, cheese"];
+
+
+alert(favFood + favFoodIngredients);
+confirm(favMovie + favMovieYear)
