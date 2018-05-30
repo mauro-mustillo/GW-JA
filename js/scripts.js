ec4c7c8de601e4171fdfddfaf1bb41c6948829f6
mauro-mustillo <mmustillo@shift3tech.com>
2018-05-29 22:38:58 -0700

    Take Hone 3

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+let fullName = prompt("What's your name?");
+let colorOptions = prompt (" What's your favorite color? You can use only : red, orange, blue, yellow, green, grey, black, cyan, magenta, purple","Your favorite color option");
+let toUppercase = colorOptions.toUpperCase();
+let nameToUppercase = fullName.toUpperCase();
+switch (toUppercase) {
+  case 'RED':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You're on Fire today!");
+  break;
+  case 'BLUE':
+  alert(nameToUppercase  + "!"  + " You like the " + toUppercase +  " Because You Love the sea");
+  break;
+  case 'GREEN':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You wish to be a Santa's Helper");
+  break;
+  case 'BLACK':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You listen METAL all day!");
+  break;
+  case 'GREY':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You're soooooo bored");
+  break;
+  case 'ORANGE':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because is the color of joy and creativity'");
+  break;
+  case 'CYAN':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You're soooooo bored");
+  break;
+  case 'MAGENTA':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because You're soooooo bored");
+  break;
+  case 'PURPLE':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because you love to play Smoke on the Water on Guitar shops");
+  break;
+  case 'YELLOW':
+  alert(nameToUppercase + "!"  + " You like the " + toUppercase +  " Because you work like a Minion");
+  break;
+  default:
+  alert(nameToUppercase +  "!"   + " You should pay attention to what I am asking you! The color you choose is not allowed")
+}
+//console.log(colorOptions === "Red");
