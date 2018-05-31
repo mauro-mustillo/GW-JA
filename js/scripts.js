cdff1d469c7d86220d1dfa873e436cab4d02dec0
mauro-mustillo <mmustillo@shift3tech.com>
2018-05-30 23:25:00 -0700

    TAKE HOME - D4

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let favMovie = ["Back to the Future","Marty McFly"];
+let secondFavMovie = ["The Departed", "Billy"];
+let thirdFavMovie = ["Gladiator", "Maximus"];
+let fourthFavMovie = ["BraveHeart", "William Wallace"];
+let fifthFavMovie = ["Godfellas", "Henry Hill"];
+
+let favoriteMovies = [];
+favoriteMovies.push(favMovie[0], secondFavMovie[0], thirdFavMovie[0], fourthFavMovie[0], fifthFavMovie[0]);
+alert(favoriteMovies);
+let bestWorstMovie = [favoriteMovies[0], favoriteMovies[4]];
+console.log(bestWorstMovie);
+alert(favoriteMovies.length)
